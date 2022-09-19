2fd63cdc98dbee3c8f9d3368a0469cdd9928cbee
iigokingii <smolik.valera@mail.ru>
2022-09-19 03:07:08 +0300

    added nested objects and overloaded operations

--- 3/lab03/lab03/Program.cs
+++ 3/lab03/lab03/Program.cs
@@ -5,6 +5,61 @@ namespace lab03
 {
     public class stack<T>
     {
+        public class Production
+        {
+            public int Id;
+            public string Name;
+            public Production()
+            {
+                Id = 0;
+            }
+            public Production(int ID,string NAME)
+            {
+                Id = ID;
+                Name = NAME;
+            }
+        }
+        class Developer
+        {
+            string fio;
+            int id;
+            int department;
+            public string FIO
+            {
+                get
+                {
+                    return fio;
+                }
+                set
+                {
+                    fio = value;
+                }
+            }
+            public int ID
+            {
+                get
+                {
+                    return id;
+                }
+                set
+                {
+                    id = ID;
+                }
+            }
+            public int Department
+            {
+                get
+                {
+                    return department;
+                }
+                set
+                {
+                    department = value  ;
+                }
+            }
+        }
+
+
         const int number = 10;
         public T[] array;
         int counter = 0;
@@ -23,7 +78,7 @@ namespace lab03
         }
         public T Pop()
         {
-            if (counter==0)
+            if (counter == 0)
             {
                 throw new Exception("cтек пуст");
             }
@@ -33,7 +88,7 @@ namespace lab03
         }
         public bool Contains(T elem)
         {
-            foreach(var arr in array)
+            foreach (var arr in array)
             {
                 if (arr.Equals(elem))
                     return true;
@@ -42,7 +97,7 @@ namespace lab03
         }
         public void Clear()
         {
-            for(int i=0; i < counter; i++)
+            for (int i = 0; i < counter; i++)
             {
                 array[i] = default(T);
             }
@@ -56,15 +111,71 @@ namespace lab03
             counter++;
             return result;
         }
-
-
-
-
-
+        public void Extract()
+        {
+            array[--counter]=default(T);
+        }
+        public bool IsEmpty()
+        {
+            if (counter == 0)
+                return true;
+            return false;
+        }
+        public static stack<T> operator +(stack<T> stack,T value) {
+            stack.Push(value);
+            return stack;
+        }
+        public static stack<T> operator --(stack<T> stack)
+        {
+            stack.Extract();
+            return stack;
+        }
+        public static bool operator true(stack<T> stack)
+        {
+            return stack.IsEmpty();
+        }
+        public static bool operator false (stack<T> stack)
+        {
+            return stack.IsEmpty();
+        }
+        /*public static stack<int> operator >(stack<int> stack,stack<int> stack1)
+        {
+            stack<int> stack3 = new stack<int>();
+            int max = 0;
+            for(int i=0;i<stack.counter;i++)
+            {
+                for (int j = 0; j < stack.counter; j++)
+                {
+                    if (max < stack.array[j])
+                        max = stack.array[j];
+                }
+                stack.array[i] = max;
+            }
+            for (int i = 0; i < stack.counter; i++)
+            {
+                stack3.array[i] = stack.array[i];
+            }
+            return stack3;
+        }
+        public static stack<int> operator <(stack<int> stack, stack<int> stack1)
+        {
+            int min = 0;
+            for (int i = 0; i < stack.counter; i++)
+            {
+                for (int j = 0; j < stack.counter; j++)
+                {
+                    if (min > stack.array[j])
+                        min = stack.array[j];
+                }
+                stack.array[i] = min;
+            }
+            for (int i = 0; i < stack.counter; i++)
+            {
+                stack1.array[i] = stack.array[i];
+            }
+            return stack1;
+        }*/
     }
-
-
-
     class Program
     {
         static void Main(string[] args)
@@ -82,9 +193,29 @@ namespace lab03
                 Console.WriteLine($"includes");
                 bool b = stack1.Contains(2);
                 int c = stack1.Peek();
-                stack1.Clear();
-                int top1 = stack1.Pop();
-                Console.WriteLine($"top:{top}");
+                int m = 412;
+
+
+                stack<int> result = stack1 + m;
+                stack1--;
+                if (stack1)
+                    Console.WriteLine("стек пуст");
+                else
+                    Console.WriteLine("стек не пуст");
+
+                stack<int> stack3 = new stack<int>();
+                //stack3= stack1 > stack3;
+                stack<string>.Production prod1 = new stack<string>.Production();
+                stack<int>.Production prod2 = new stack<int>.Production(1,"GoKing");
+                
+
+
+
+                //stack1.Clear();
+                //int top1 = stack1.Pop();
+                //Console.WriteLine($"top:{top}");
+
+
             }
             catch(Exception e)
             {
