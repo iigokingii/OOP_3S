673b04fd9973969b90b910c537cf8c2a45b79a0f
iigokingii <smolik.valera@mail.ru>
2022-10-24 00:32:26 +0300

    added interface and some interface members are writen

---
+++ 9/lab09/lab09/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Collections;
+namespace lab09
+{
+    class Services : IOrderedDictionary,
+    {
+        ArrayList _people;
+        Queue type;
+        public void Add(object key, object value)
+        {
+            type.Enqueue(new DictionaryEntry(key, value));
+        }
+        public int Count
+        {
+            get
+            {
+                return type.Count;
+            }
+        }
+        public bool IsSynchronized
+        {
+            get
+            {
+                return type.IsSynchronized;
+            }
+        }
+        public object SyncRoot
+        {
+            get
+            {
+                return type.SyncRoot;
+            }
+        }
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
