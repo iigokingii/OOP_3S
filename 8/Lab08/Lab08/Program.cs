d3a162c4f2cd6ab425bcfe1cc84bec7fc31f4c09
iigokingii <smolik.valera@mail.ru>
2022-10-23 15:47:36 +0300

    added class Language,modified some methods

--- 8/Lab08/Lab08/Program.cs
+++ 8/Lab08/Lab08/Program.cs
@@ -2,45 +2,63 @@
 using System.Collections.Generic;
 namespace Lab08
 {
+    enum operation
+    {
+        addOperation,
+        addProperty
+    }
     class Programmer
     {
-        public delegate void method(string _property);
-        public event method rename;
-        public event method newProperty;
+        string name;
+        string course;
+        string speciality;
+        public delegate void method(string tmp);
+        public event method? rename;
+        public event method? newProperty;
+        public void Rename(string _name) => rename?.Invoke(_name);
+        public void newproperty(string _property) => newProperty?.Invoke(_property);
+        public void newVersion(string _version) => rename?.Invoke(_version);
+        public Programmer(string _name, string _course,string _speciality)
+        {
+            name = _name;
+            course = _course;
+            speciality = _speciality;
+        }
+
+    }
+
+    class Language
+    {
         string name;
         List<string> properties = new List<string>();
-        float version;
+        string version;
         List<string> operations = new List<string>();
-        int counter = 0;
-        public Programmer(string _name, string _property, float _version, string _operation)
+        
+        public Language(string _name, string _property, string _version, string _operation)
         {
             name = _name;
             properties.Add(_property);
             version = _version;
             operations.Add(_operation);
-            counter++;
         }
 
+
         public void Rename(string _name)
-        {
-            rename(_name);
-        }
-        public void newName(string _name)
         {
             Console.WriteLine("Метод rename");
             name = _name;
         }
-        public void newproperty(string _property)
+        public void NewVersion(string _version)
         {
-            newProperty(_property);   
-        }
-        public void NewProperty(string _property)
-        {
-            Console.WriteLine("метод NewProperty");
-            properties.Add(_property);
-            counter++;
+            Console.WriteLine("Метод newversion");
+            version = _version;
         }
 
+        public void addProperty(string _property)
+        {
+            Console.WriteLine("Метод addProperty");
+            properties.Add(_property);
+        }
     }
 
 
@@ -49,11 +67,27 @@ namespace Lab08
     {
         static void Main(string[] args)
         {
-            Programmer lang1 = new Programmer("c#", "Полиморфизм", 123, "+");
-            lang1.rename += lang1.newName;
-            lang1.newProperty += lang1.NewProperty;
-            lang1.Rename("c++");
-            lang1.newproperty("Абстракция");
+            Programmer programmer1 = new Programmer("Ваня","3","ИСиТ");
+            Language lang1 = new Language("c#", "Полиморфизм", "11", "+");
+
+            programmer1.rename += lang1.Rename;
+            programmer1.Rename("java");
+            programmer1.rename -= lang1.Rename;
+
+            programmer1.newProperty += lang1.addProperty;         
+            programmer1.newproperty("Абстракция");
+
+            programmer1.rename += lang1.NewVersion;
+            programmer1.newVersion("10"); 
+
+            Language lang2 = new Language("java", "Объектно-ориентированный", "16", "-");
+            programmer1.newProperty += lang2.addProperty;
+            programmer1.newproperty("Инкапсуляция");
+
+            Language lang3 = new Language("с++", "Статически типизированный", "16.7", "/");
+
+
+
 
 
 
