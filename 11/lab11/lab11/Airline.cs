901f97f865b6446357bfa8be2bde8c057e9b1d0a
iigokingii <smolik.valera@mail.ru>
2022-11-03 13:09:04 +0300

    added Creating method in class Reflector,class Airline moved to the file Airline.cs

---
+++ 11/lab11/lab11/Airline.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab11
+{
+    class Airline
+    {
+        readonly string id = Guid.NewGuid().ToString();
+        static int counter = 0;
+        static string type;
+        string pointOfDeparture;
+        int number;
+        const string time = "21:35";
+        string day;
+        public Airline()
+        {
+            pointOfDeparture = "Minsk";
+            number = 222123;
+            day = "wed";
+            counter++;
+        }
+        public Airline(string point, int number, string day)
+        {
+            this.pointOfDeparture = point;
+            this.number = number;
+            this.day = day;
+            counter++;
+        }
+        public Airline(int number, string day)
+        {
+            this.pointOfDeparture = "vileyka";
+            this.number = number;
+            this.day = day;
+            counter++;
+        }
+        static Airline()
+        {
+            type = "luxe";
+        }
+        private Airline(string point, int number, string day, string constructor)
+        {
+            this.pointOfDeparture = point;
+            this.number = number;
+            this.day = day;
+        }
+        public override int GetHashCode()
+        {
+            int Hash;
+            Hash = (this.number - 12) / 123;
+            Console.WriteLine($"Hashcode:{Hash}");
+            return Hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Airline m = obj as Airline;
+            if (m as Airline == null)
+                return false;
+            return m.day == this.day && m.id == this.id && m.number == this.number && m.pointOfDeparture == this.pointOfDeparture;
+        }
+
+        public override string ToString()
+        {
+            return $"id:{id} type:{type} point of departure:{pointOfDeparture} number:{number} time:{time} day:{day} №{counter}";
+        }
+        public int Number
+        {
+            set
+            {
+                number = value;
+            }
+            get
+            {
+                return number;
+            }
+        }
+        public string PointOfDeparture
+        {
+            set
+            {
+                pointOfDeparture = value;
+            }
+            get
+            {
+                return pointOfDeparture;
+            }
+        }
+        public string Day
+        {
+            set
+            {
+                day = value;
+            }
+            get
+            {
+                return day;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+        public string Time
+        {
+
+            get
+            {
+                return time;
+            }
+        }
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+        public int Counter
+        {
+            get
+            {
+                return counter;
+            }
+        }
+        public static void Print(Airline airline) => Console.WriteLine($"id:{airline.Id} type:{type} point of departure:{airline.pointOfDeparture} number:{airline.number} time:{time} day:{airline.day} №{counter}");
+    }
+
+}
--- 11/lab11/lab11/Program.cs
+++ 11/lab11/lab11/Program.cs
@@ -7,10 +7,10 @@ using System.Xml.Linq;
 
 namespace lab11
 {
-    static class Reflector
+    static class Reflector<T>
     {
-        static bool Flag=false;
-        static public void WriteInit(IEnumerable<string> stroke,string rootNode,string name,string secondNode,string thirdNode)
+        static bool Flag = false;
+        static public void WriteInit(IEnumerable<string> stroke, string rootNode, string name, string secondNode, string thirdNode)
         {
             XDocument xdoc = new XDocument();
             XElement type = new XElement($"{name}");
@@ -26,14 +26,14 @@ namespace lab11
             xdoc.Add(reflector);
             xdoc.Save("Reflector.xml");
         }
-        static public void Write(IEnumerable<string> stroke,string name,string secondNode,string thirdNode)//класс (xmlnode)
+        static public void Write(IEnumerable<string> stroke, string name, string secondNode, string thirdNode)//класс (xmlnode)
         {
             XmlDocument xdoc = new XmlDocument();
             xdoc.Load("Reflector.xml");
 
             XmlElement? root = (XmlElement)xdoc.SelectSingleNode($"Reflector/{name}");
             XmlElement xmlElement = xdoc.CreateElement($"{secondNode}");    //новый элемент(3-ий)
-            foreach(string tmp in stroke)
+            foreach (string tmp in stroke)
             {
                 XmlElement element = xdoc.CreateElement($"{thirdNode}");
                 XmlText text = xdoc.CreateTextNode($"{tmp}");
@@ -66,50 +66,50 @@ namespace lab11
 
         static public IEnumerable<string> Interfaces(Type obj)
         {
-            Console.WriteLine("\nInterfaces of {0}",obj.Name);
-          
+            Console.WriteLine("\nInterfaces of {0}", obj.Name);
+
             IEnumerable<string> stroke = obj.GetInterfaces()
                                             .Where(n => n is Type)
                                             .Select(n => ((Type)n).Name);
-            foreach(string str in stroke)
+            foreach (string str in stroke)
             {
-                Console.WriteLine(str);  
+                Console.WriteLine(str);
             }
             if (!Flag)
             {
                 WriteInit(stroke, "Reflector", $"{obj.Name}", "Interfaces", "Interface");
                 Flag = true;
-            }               
+            }
             else
-                WriteN(stroke,$"{obj.Name}","Interfaces", "Interface");
+                WriteN(stroke, $"{obj.Name}", "Interfaces", "Interface");
             return stroke;
         }
 
         static public IEnumerable<string> Methods(Type obj)
         {
-            Console.WriteLine("\n methods of {0}",obj.Name);
-            IEnumerable<string> stroke =(IEnumerable<string>)obj.GetMethods(BindingFlags.Public|BindingFlags.Instance)
+            Console.WriteLine("\n methods of {0}", obj.Name);
+            IEnumerable<string> stroke = (IEnumerable<string>)obj.GetMethods(BindingFlags.Public | BindingFlags.Instance)
                                             .Where(n => n is MethodInfo)
                                             .Select(n => ((MethodInfo)n).Name);
             foreach (string str in stroke)
             {
                 Console.WriteLine(str);
             }
-            Write(stroke,$"{ obj.Name}", "Methods", "Method");
+            Write(stroke, $"{ obj.Name}", "Methods", "Method");
             return stroke;
         }
 
         static public IEnumerable<string> PropertiesAndFields(Type obj)
         {
             Console.WriteLine("\nProperties and fields of {0}", obj.Name);
-            IEnumerable<string> stroke1 = (IEnumerable<string>)obj .GetProperties()
+            IEnumerable<string> stroke1 = (IEnumerable<string>)obj.GetProperties()
                                                                   .Where(n => n is PropertyInfo)
                                                                   .Select(n => ((PropertyInfo)n).Name);      //протестить
             IEnumerable<string> stroke2 = (IEnumerable<string>)obj.GetFields()
                                                                   .Where(n => n is FieldInfo)
                                                                   .Select(n => ((FieldInfo)n).Name);
             IEnumerable<string> stroke = stroke1.Concat(stroke2);
-            foreach(string tmp in stroke)
+            foreach (string tmp in stroke)
             {
                 Console.WriteLine(tmp);
             }
@@ -120,7 +120,7 @@ namespace lab11
         static public void Assembly(Assembly assembly, Type obj)
         {
             Console.WriteLine("Name of Assembly,classes: ");
-            string tmp="";
+            string tmp = "";
             Console.WriteLine(assembly.FullName);
             foreach (Module module in assembly.GetModules())
             {
@@ -143,14 +143,14 @@ namespace lab11
         }
         static public bool ConstructorInfo(Type obj)    //доп тесты
         {
-            bool flag=false;
-            foreach(ConstructorInfo constructor in obj.GetConstructors())
+            bool flag = false;
+            foreach (ConstructorInfo constructor in obj.GetConstructors())
             {
                 if (constructor.IsPublic)
                 {
                     flag = true;
                     break;
-                }     
+                }
             }
             XmlDocument xml = new XmlDocument();
             xml.Load("Reflector.xml");
@@ -167,19 +167,19 @@ namespace lab11
             return flag;
         }
 
-        static public void MethodsWithParm(Type obj,string param)
+        static public void MethodsWithParm(Type obj, string param)
         {
-            Console.WriteLine("\nMethods of class {0},that contain \"{1}\":",obj.Name,param);
+            Console.WriteLine("\nMethods of class {0},that contain \"{1}\":", obj.Name, param);
             XmlDocument xml = new XmlDocument();
             xml.Load("Reflector.xml");
             XmlElement? root = (XmlElement)xml.SelectSingleNode($"Reflector/{obj.Name}");
             XmlElement MethodWithParm = xml.CreateElement("MethodsWithParm");
             foreach (MethodInfo MI in obj.GetMethods())
             {
-                ParameterInfo[]PI= MI.GetParameters();
+                ParameterInfo[] PI = MI.GetParameters();
                 for (int i = 0; i < PI.Length; i++)
                 {
-                    if (PI[i].ParameterType.Name ==param)
+                    if (PI[i].ParameterType.Name == param)
                     {
                         XmlElement Meth = xml.CreateElement("Method");
                         XmlText text = xml.CreateTextNode($"{MI.ReturnType.Name} {MI.Name} ({PI[i].ParameterType.Name} {PI[i].Name})");
@@ -192,139 +192,57 @@ namespace lab11
             root?.AppendChild(MethodWithParm);
             xml.Save("Reflector.xml");
         }
+        static public void Invoke(Airline ob,string name)
+        {
+            var Equal = typeof(Airline).GetMethod("name");
+
+
+
+            /*Type obj = typeof(Airline);
+            XmlDocument xml = new XmlDocument();
+            xml.Load("Reflector.xml");
+            XmlElement? root =xml.DocumentElement;
+            foreach(XmlElement element in root)
+            {
+                if (element.Name == obj.Name)
+                {
+                    foreach(XmlNode node in element.ChildNodes)
+                    {
+                        if (node.Name == "MethodsWithParm")
+                        {
+                            foreach(XmlElement el in node)
+                            {
+                                if (el.InnerText.Contains(name))
+                                {
+
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+            }*/
+
+        }
+
+
+        static public T Create(T typ)
+        {
+            T result;
+            Type ty = typeof(T);
+            var tmp = ty.GetConstructors();
+            var constructor = tmp.OrderBy(n => n.GetParameters().Length).FirstOrDefault();
+            if (constructor != null)
+                constructor.Invoke(typ, parameters: null);
+            else
+                return typ;
+            result = typ;
+            return result;
+        }
+
+
     }
-
-    class Airline
-    {
-        readonly string id = Guid.NewGuid().ToString();
-        static int counter = 0;
-        static string type;
-        string pointOfDeparture;
-        int number;
-        const string time = "21:35";
-        string day;
-        public Airline()
-        {
-            pointOfDeparture = "Minsk";
-            number = 222123;
-            day = "wed";
-            counter++;
-        }
-        public Airline(string point, int number, string day)
-        {
-            this.pointOfDeparture = point;
-            this.number = number;
-            this.day = day;
-            counter++;
-        }
-        public Airline(int number, string day)
-        {
-            this.pointOfDeparture = "vileyka";
-            this.number = number;
-            this.day = day;
-            counter++;
-        }
-        static Airline()
-        {
-            type = "luxe";
-        }
-        private Airline(string point, int number, string day, string constructor)
-        {
-            this.pointOfDeparture = point;
-            this.number = number;
-            this.day = day;
-        }
-        public override int GetHashCode()
-        {
-            int Hash;
-            Hash = (this.number - 12) / 123;
-            Console.WriteLine($"Hashcode:{Hash}");
-            return Hash;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-                return false;
-            Airline m = obj as Airline;
-            if (m as Airline == null)
-                return false;
-            return m.day == this.day && m.id == this.id && m.number == this.number && m.pointOfDeparture == this.pointOfDeparture;
-        }
-
-        public override string ToString()
-        {
-            return $"id:{id} type:{type} point of departure:{pointOfDeparture} number:{number} time:{time} day:{day} №{counter}";
-        }
-        public int Number
-        {
-            set
-            {
-                number = value;
-            }
-            get
-            {
-                return number;
-            }
-        }
-        public string PointOfDeparture
-        {
-            set
-            {
-                pointOfDeparture = value;
-            }
-            get
-            {
-                return pointOfDeparture;
-            }
-        }
-        public string Day
-        {
-            set
-            {
-                day = value;
-            }
-            get
-            {
-                return day;
-            }
-        }
-
-        public string Id
-        {
-            get
-            {
-                return id;
-            }
-        }
-        public string Time
-        {
-
-            get
-            {
-                return time;
-            }
-        }
-        public string Type
-        {
-            get
-            {
-                return type;
-            }
-        }
-        public int Counter
-        {
-            get
-            {
-                return counter;
-            }
-        }
-        public static void Print(Airline airline) => Console.WriteLine($"id:{airline.Id} type:{type} point of departure:{airline.pointOfDeparture} number:{airline.number} time:{time} day:{airline.day} №{counter}");
-    }
-
-
-
-
     class Program
     {
         static void Main(string[] args)
@@ -333,44 +251,52 @@ namespace lab11
 
             Type t = typeof(Int32);
 
-            Reflector.Interfaces(t);
+            Reflector<Int32>.Interfaces(t);
 
-            Reflector.Methods(t);
+            Reflector<Int32>.Methods(t);
 
-            Reflector.PropertiesAndFields(t);
+            Reflector<Int32>.PropertiesAndFields(t);
 
-            Reflector.MethodsWithParm(t, "Int32");
+            Reflector<Int32>.MethodsWithParm(t, "Int32");
 
-            if (Reflector.ConstructorInfo(t))
+            if (Reflector<Int32>.ConstructorInfo(t))
                 Console.WriteLine("Содержит публичный конструктор");
             else
                 Console.WriteLine("Не содержит публичный конструктор");
-            Reflector.Assembly(assembly, t);
+            Reflector<Int32>.Assembly(assembly, t);
 
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nCледующий класс\n") ;
             Console.ResetColor();
             
 
-            Airline airline = new Airline();
+            Airline airline1 = new Airline();
+            Airline airline2 = new Airline();
             Type type = typeof(Airline);
-            Reflector.Interfaces(type);
+            Reflector<Airline>.Interfaces(type);
 
-            Reflector.Methods(type);
+            Reflector<Airline>.Methods(type);
 
-            Reflector.PropertiesAndFields(type);
+            Reflector<Airline>.PropertiesAndFields(type);
 
-            Reflector.MethodsWithParm(type, "Object");
+            Reflector<Airline>.MethodsWithParm(type, "Object");
 
-            if (Reflector.ConstructorInfo(t))
+            if (Reflector<Airline>.ConstructorInfo(t))
                 Console.WriteLine("Содержит публичный конструктор");
             else
                 Console.WriteLine("Не содержит публичный конструктор");
 
             Console.WriteLine();
-            Reflector.Assembly(assembly, type);
+            Reflector<Airline>.Assembly(assembly, type);
 
+            /*var Equal = typeof(Airline).GetMethod("Equals");
+            bool b = (bool)Equal.Invoke(airline1, new object []{airline2});
+            Reflector.Invoke(airline1,"Equals");*/
+            Airline airline= new Airline();
+            Type type1 = typeof(Airline);
+           Airline air=Reflector<Airline>.Create(airline);
 
         }
     }
 }
+
