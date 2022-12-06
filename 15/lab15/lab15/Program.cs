2e3776cb4be0488e7f5cfa23ffe7dc333308e1d2
iigokingii <smolik.valera@mail.ru>
2022-12-06 03:12:56 +0300

    added parallel for; parallel foreach ; parallel invoke

--- 15/lab15/lab15/Program.cs
+++ 15/lab15/lab15/Program.cs
@@ -9,6 +9,8 @@ namespace lab15
     {
         public void Task1()
         {
+            Thread.Sleep(600);
+            Console.WriteLine("---Task 1---");
             Stopwatch stopWatch = new Stopwatch();
             
             Console.WriteLine("введите n: ");
@@ -43,6 +45,8 @@ namespace lab15
         }
         public void Task2()
         {
+            Thread.Sleep(500);
+            Console.WriteLine("---Task 2---");
             CancellationTokenSource source = new CancellationTokenSource();
             CancellationToken token = source.Token;
             Task task = new Task(() => {
@@ -91,6 +95,8 @@ namespace lab15
 
         public void Task3()
         {
+            Thread.Sleep(400);
+            Console.WriteLine("---Task 3---");
             int z1=2, z2=4;
             Task<int> sum = new Task<int>(() => {
                 Console.WriteLine("task Sum");
@@ -119,9 +125,68 @@ namespace lab15
             int Square(int x) => x * x;
             int Volume(int a, int b, int c) => a * b * c;
         }
+        public void Task4()
+        {
+            Thread.Sleep(300);
+            Console.WriteLine("---TASK 4---");
+            Task<int> task = new Task<int>(() => Mul(4, 5));
+            Task print = task.ContinueWith(t => PrintResult(t.Result));
+            task.Start();
+            print.Wait();
+            int Mul(int a, int b) => a * b;
+            void PrintResult(int sum) => Console.WriteLine($"Mul: {sum}");
+            
+        }
+        public void Task5()
+        {
+            Thread.Sleep(200);
+            Console.WriteLine("---Task 5---");
+            int[] arr1 = new int[10000];
+            int[] arr2 = new int[10000];
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Parallel.For: ");
+            Parallel.For(1, arr1.Length,t=>arr1[t]=t);
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            Console.WriteLine("RunTime " + ts + "\n");
 
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+            Console.WriteLine("Standart for: ");
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr2[i] = i;
+            }
+            stopwatch2.Stop();
+            TimeSpan ts2 = stopwatch2.Elapsed;
+            Console.WriteLine("RunTime "+ts2 +"\n");
+            //foreach
+            Stopwatch stopwatch4 = new Stopwatch();
+            stopwatch4.Start();
+            Parallel.ForEach<int>(new List<int>() { 1, 2, 3, 4 },Square);
+            stopwatch4.Stop();
+            Console.WriteLine("Parallel.Foreach: " + stopwatch4.Elapsed);
 
-
+            Stopwatch stopwatch5 = new Stopwatch();
+            stopwatch5.Start();
+            foreach (var m in new List<int>() { 1, 2, 3, 4 })
+            {
+                Square(m);
+            }
+            stopwatch5.Stop();
+            Console.WriteLine("foreach: " + stopwatch5.Elapsed);
+            Console.WriteLine();
+            static void Square(int a) => Console.WriteLine($"Square: {a*a}");
+        }
+        public void Task6()
+        {
+            Console.WriteLine("[---Task 6---]");
+            Parallel.Invoke(
+               Task5,
+               Task4,
+               Task3);
+        }
     }
 
 
@@ -132,15 +197,20 @@ namespace lab15
         static void Main(string[] args)
         {
             Tasks tasks = new Tasks();
-            Task task1 = new Task(tasks.Task1);
-            Console.WriteLine($"Id: {task1.Id};\nStatus: {task1.Status};\nCompleted? ({task1.IsCompleted})\n");
-            task1.Start();
-            
-            task1.Wait();
+            /*  Task task1 = new Task(tasks.Task1);
+              Console.WriteLine($"Id: {task1.Id};\nStatus: {task1.Status};\nCompleted? ({task1.IsCompleted})\n");
+              task1.Start();
+              task1.Wait();
 
-            tasks.Task2();
+              tasks.Task2();
 
-            tasks.Task3();
+              tasks.Task3();
+
+              tasks.Task4();
+
+              tasks.Task5();*/
+            tasks.Task6();
+
             Console.WriteLine("Main");
         }
     }
