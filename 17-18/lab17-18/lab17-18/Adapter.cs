7778d8503f8d26751c16a25448e7b21c32ea159c
iigokingii <smolik.valera@mail.ru>
2022-12-27 02:53:15 +0300

    added adapter for currency

--- 17-18/lab17-18/lab17-18/Adapter.cs
+++ 17-18/lab17-18/lab17-18/Adapter.cs
@@ -4,73 +4,50 @@ using System.Text;
 
 namespace lab17_18
 {
-    /*
-     class Program
- {
-     static void Main(string[] args)
-     {
-         // путешественник
-         Driver driver = new Driver();
-         // машина
-         Auto auto = new Auto();
-         // отправляемся в путешествие
-         driver.Travel(auto);
-         // встретились пески, надо использовать верблюда
-         Camel camel = new Camel();
-         // используем адаптер
-         ITransport camelTransport = new CamelToTransportAdapter(camel);
-         // продолжаем путь по пескам пустыни
-         driver.Travel(camelTransport);
+    interface ICurrencyEuro
+    {
+        void PayInEuro();
+    }
+    
+    class Euro : ICurrencyEuro
+    {
+        public void PayInEuro()
+        {
+            Console.WriteLine("Номер был оплачен в Евро ");
+        }
+    }
 
-         Console.Read();
-     }
- }
- interface ITransport
- {
-     void Drive();
- }
- // класс машины
- class Auto : ITransport
- {
-     public void Drive()
-     {
-         Console.WriteLine("Машина едет по дороге");
-     }
- }
- class Driver
- {
-     public void Travel(ITransport transport)
-     {
-         transport.Drive();
-     }
- }
- // интерфейс животного
- interface IAnimal
- {
-     void Move();
- }
- // класс верблюда
- class Camel : IAnimal
- {
-     public void Move()
-     {
-         Console.WriteLine("Верблюд идет по пескам пустыни");
-     }
- }
- // Адаптер от Camel к ITransport
- class CamelToTransportAdapter : ITransport
- {
-     Camel camel;
-     public CamelToTransportAdapter(Camel c)
-     {
-         camel = c;
-     }
+    class Payer
+    {
+        public void Pay(ICurrencyEuro currency)
+        {
+            currency.PayInEuro();
+        }
+    } 
 
-     public void Drive()
-     {
-         camel.Move();
-     }
- }
+    interface ICurrencyBYN
+    {
+        void PayInBYN();
+    }
 
-     */
+    class BYN : ICurrencyBYN
+    {
+        public void PayInBYN()
+        {
+            Console.WriteLine("Номер был оплачен в Белорусских рублях");
+        }
+    }
+
+    class BYNToEuro :ICurrencyEuro
+    {
+        BYN byn;
+        public BYNToEuro(BYN _byn)
+        {
+            byn = _byn;
+        }
+        public void PayInEuro()
+        {
+            byn.PayInBYN();
+        }
+    }
 }
--- 17-18/lab17-18/lab17-18/Program.cs
+++ 17-18/lab17-18/lab17-18/Program.cs
@@ -7,33 +7,37 @@ namespace lab17_18
     {
         static void Main(string[] args)
         {
-           /* Console.Clear();
+            /* Console.Clear();
 
-            *//*Console.Clear();*//*
-            //Abstract Factory
-            Score score = new Score(new Factory());
-            score.Run();
+             *//*Console.Clear();*//*
+             //Abstract Factory
+             Score score = new Score(new Factory());
+             score.Run();
 
-            //Builder
-            ConcreteBuilder builder = new ConcreteBuilder();
-            Director director = new Director(builder);
-            director.Construct();
-            Request req = builder.GetResult();
-            score.RunWithRequest(req);
+             //Builder
+             ConcreteBuilder builder = new ConcreteBuilder();
+             Director director = new Director(builder);
+             director.Construct();
+             Request req = builder.GetResult();
+             score.RunWithRequest(req);
 
-            //Prototype
-            IFigure requestClone = req.Clone();
-            requestClone.GetInfo();
+             //Prototype
+             IFigure requestClone = req.Clone();
+             requestClone.GetInfo();
 
-            //Singleton
-            Settings setting = new Settings();
-            setting.Launch("green", "Cyan");
-            Settings settings = new Settings();
-            settings.Launch("red", "Cyan");*/
+             //Singleton
+             Settings setting = new Settings();
+             setting.Launch("green", "Cyan");
+             Settings settings = new Settings();
+             settings.Launch("red", "Cyan");*/
 
             //Adapter
-
-
+            Payer payer = new Payer();
+            Euro euro = new Euro();
+            payer.Pay(euro);
+            BYN byn = new BYN();
+            ICurrencyEuro currency = new BYNToEuro(byn);
+            payer.Pay(currency);
 
             //Decorator
             Room room = new CentralLocation();
